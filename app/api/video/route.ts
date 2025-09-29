fa58dabe58c45f25951ec577858272e5248a18a6
paramjeetn <paramjeetnpradhan@gmail.com>
2025-09-30 01:59:06 +0530

    dynamic tinyurls

---
+++ app/api/video/route.ts
@@ -0,0 +1,5 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  return NextResponse.redirect('https://drive.google.com/file/d/1FgP4Wb4D2_TJ9vXzOqsKIKDmd5qm7zrZ/view?usp=sharing');
+}
